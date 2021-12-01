cbb43f78b9566fb4167c35b43d63228612511f91
takumi1213 <shinnsekai5@gmail.com>
2021-12-01 21:42:14 +0900

    initial commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,17 @@
+// const { environment } = require('@rails/webpacker')
+
+// module.exports = environment
+
+
+const { environment } = require('@rails/webpacker')
+ 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+ 
+module.exports = environment
