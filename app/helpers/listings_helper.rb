f00c987dd2acb1f34af0742c9b629a178778201a
takumi1213 <91010905+takumi1213@users.noreply.github.com>
2021-12-07 22:40:49 +0900

    Create listings (#5)
    * Create  listing
    
    * Change listing

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,16 @@
 module ListingsHelper
+
+  def is_filled?(entry)
+    if entry.present?
+      content_tag :i, nil, class: 'fa fa-check text-success'
+    else
+      content_tag :i, nil, class: 'fa fa-times text-danger'
+    end
+  end
+
+  def is_ready_to_publish(listing)
+    listing.address.present? && listing.listing_description.present? && listing.price.present? && listing.listing_name.present? #&& listing.images.present?
+  end
+
 end
+
